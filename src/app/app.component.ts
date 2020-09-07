314191e5cfe9af23a65b6d13f9d86079c54b18d1
Steven Penava Jr <stevenpenava@Stevens-MBP.lan1>
2020-09-06 22:33:14 -0400

    Cleanup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,3 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-// import * as firebase from 'firebase/app';
-// import 'firebase/analytics';
-// import 'firebase/auth';
-// import 'firebase/firestore';
-
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
