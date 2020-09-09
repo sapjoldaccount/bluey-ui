b13be2658cd3fec744389661c866b4f09235b447
Steven Penava Jr <stevenpenava@Stevens-MBP.attlocal.net>
2020-09-09 12:07:58 -0400

    Video container mobile optimize.

--- src/app/core/components/landing-top-video/landing-top-video.component.ts
+++ src/app/core/components/landing-top-video/landing-top-video.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ResponsiveService } from 'src/app/shared/services/responsive/responsive.service';
+import { ScreenSize } from 'src/app/shared/enums/screen-size.enum';
 
 @Component({
   selector: 'app-landing-top-video',
@@ -9,6 +10,7 @@ import { ResponsiveService } from 'src/app/shared/services/responsive/responsive
 export class LandingTopVideoComponent implements OnInit {
 
   screenSize$ = this.responsiveService.screenSize$;
+  screenSizes = ScreenSize;
 
   constructor(private responsiveService: ResponsiveService) { }
 
