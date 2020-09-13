48ff3591c84cae98d93fd89646a9ac6155c977ee
Steven Penava Jr <steven.penava@gmail.com>
2020-09-13 17:34:36 -0400

    Parameterized item card.

--- src/app/core/components/shop-item-card/shop-item-card.component.ts
+++ src/app/core/components/shop-item-card/shop-item-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shop-item-card',
   templateUrl: './shop-item-card.component.html',
-  styleUrls: ['./shop-item-card.component.scss']
+  styleUrls: ['./shop-item-card.component.scss'],
 })
 export class ShopItemCardComponent implements OnInit {
+  @Input() title: string;
+  @Input() price: string; // for now
+  @Input() description: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
