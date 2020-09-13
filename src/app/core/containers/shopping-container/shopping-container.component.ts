e5611b02f792b53046f25a81e77b90ae689b7bc4
Steven Penava Jr <steven.penava@gmail.com>
2020-09-13 16:49:52 -0400

    Initial design of shop page.

--- src/app/core/containers/shopping-container/shopping-container.component.ts
+++ src/app/core/containers/shopping-container/shopping-container.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLinkActive } from '@angular/router';
+import { ResponsiveService } from 'src/app/shared/services/responsive/responsive.service';
 
 @Component({
   templateUrl: './shopping-container.component.html',
-  styleUrls: ['./shopping-container.component.scss']
+  styleUrls: ['./shopping-container.component.scss'],
 })
 export class ShoppingContainerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
