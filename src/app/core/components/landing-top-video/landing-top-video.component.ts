6b6607f57beaffd82501c8c541ada52602cde15d
Steven Penava Jr <steven.penava@gmail.com>
2020-09-14 21:14:05 -0400

    Added video path.

--- src/app/core/components/landing-top-video/landing-top-video.component.ts
+++ src/app/core/components/landing-top-video/landing-top-video.component.ts
@@ -1,18 +1,20 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ResponsiveService } from 'src/app/shared/services/responsive/responsive.service';
 import { ScreenSize } from 'src/app/shared/enums/screen-size.enum';
+import { CDN_BASE_URL, CDN_VIDEO_PATH } from '../../consts/cdn.consts';
 
 @Component({
   selector: 'app-landing-top-video',
   templateUrl: './landing-top-video.component.html',
-  styleUrls: ['./landing-top-video.component.scss']
+  styleUrls: ['./landing-top-video.component.scss'],
 })
 export class LandingTopVideoComponent implements OnInit {
-
   screenSize$ = this.responsiveService.screenSize$;
   screenSizes = ScreenSize;
 
-  constructor(private responsiveService: ResponsiveService) { }
+  videoUrl = `${CDN_BASE_URL}${CDN_VIDEO_PATH}`;
+
+  constructor(private responsiveService: ResponsiveService) {}
 
   /**
    * Document scroll listener
@@ -22,10 +24,11 @@ export class LandingTopVideoComponent implements OnInit {
   @HostListener('document:scroll', ['$event'])
   onScroll(event): void {
     const yPixelOffset = window.pageYOffset;
-    document.getElementById('top').style.opacity = (0 + yPixelOffset / window.innerHeight).toString();
+    document.getElementById('top').style.opacity = (
+      0 +
+      yPixelOffset / window.innerHeight
+    ).toString();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
