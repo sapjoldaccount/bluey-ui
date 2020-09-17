63892dda19c6ccb51418e65834b8caeeb9620a54
Steven Penava Jr <steven.penava@gmail.com>
2020-09-16 21:02:51 -0400

    Added improved video.

--- src/app/core/consts/cdn.consts.ts
+++ src/app/core/consts/cdn.consts.ts
@@ -1,2 +1,2 @@
 export const CDN_BASE_URL = 'https://bluey-shop.s3.amazonaws.com';
-export const CDN_VIDEO_PATH = '/webm/video_bg.webm';
+export const CDN_VIDEO_PATH = '/mp4/video_trimmed_small.mp4';
