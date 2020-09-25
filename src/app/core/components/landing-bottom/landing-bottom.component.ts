00c3cadb280f496d00df66c6009d9e2987bfa62a
Steven Penava Jr <steven.penava@gmail.com>
2020-09-25 16:06:29 -0400

    Added footer styling and images to featured decks.

--- src/app/core/components/landing-bottom/landing-bottom.component.ts
+++ src/app/core/components/landing-bottom/landing-bottom.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { CDN_BASE_URL } from '../../consts/cdn.consts';
 
 @Component({
   selector: 'app-landing-bottom',
   templateUrl: './landing-bottom.component.html',
-  styleUrls: ['./landing-bottom.component.scss']
+  styleUrls: ['./landing-bottom.component.scss'],
 })
 export class LandingBottomComponent implements OnInit {
+  cdnBaseUrl = CDN_BASE_URL;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/core/components/product-preview/product-preview.component.ts
+++ src/app/core/components/product-preview/product-preview.component.ts
@@ -3,16 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-preview',
   templateUrl: './product-preview.component.html',
-  styleUrls: ['./product-preview.component.scss']
+  styleUrls: ['./product-preview.component.scss'],
 })
 export class ProductPreviewComponent implements OnInit {
-
   @Input() name: string;
   @Input() hoverText: string;
+  @Input() imgSrc: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
