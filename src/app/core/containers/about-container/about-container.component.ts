27527fc31733f4ceae74f10ee4192e4d71681624
Steven Penava Jr <steven.penava@gmail.com>
2020-09-26 18:34:17 -0400

    Added about page.

--- src/app/core/containers/about-container/about-container.component.ts
+++ src/app/core/containers/about-container/about-container.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CDN_BASE_URL } from '../../consts/cdn.consts';
 
 @Component({
   templateUrl: './about-container.component.html',
-  styleUrls: ['./about-container.component.scss']
+  styleUrls: ['./about-container.component.scss'],
 })
 export class AboutContainerComponent implements OnInit {
+  cdnBaseUrl = CDN_BASE_URL;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
