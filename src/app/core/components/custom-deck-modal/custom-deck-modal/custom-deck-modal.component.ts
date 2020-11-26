b5f466537da0315e1f36134df893f387c629fee8
Steven Penava Jr <steven.penava@gmail.com>
2020-11-26 14:59:11 -0500

    Color toggling works. Now to separate all the colors...

--- src/app/core/components/custom-deck-modal/custom-deck-modal/custom-deck-modal.component.ts
+++ src/app/core/components/custom-deck-modal/custom-deck-modal/custom-deck-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MDBModalRef } from 'angular-bootstrap-md';
+import { ColorEvent } from 'ngx-color';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BehaviorSubject } from 'rxjs';
 import { COLOR_ROWS } from 'src/app/core/consts/colors.consts';
@@ -28,6 +29,8 @@ export class CustomDeckModalComponent implements OnInit {
 
   colorRows = COLOR_ROWS;
 
+  selectedColors = [];
+
   getDeckNamErrorMsg(): string {
     if (this.deckTitle.hasError('required')) {
       return 'You must enter a name for your deck';
@@ -54,5 +57,29 @@ export class CustomDeckModalComponent implements OnInit {
 
   onBack(): void {
     this.viewingSummary.next(false);
+  }
+
+  /**
+   * Handle toggling colors
+   * @param event - contains color item and js event
+   */
+  handleChange(event): void {
+    const target =
+      event.$event.target ||
+      event.$event.srcElement ||
+      event.$event.currentTarget;
+    if (this.selectedColors.includes(event.color.hex)) {
+      target.style['box-shadow'] = `${event.color.hex} 0px 0px 0px 14px inset`;
+      this.selectedColors = this.selectedColors.filter(
+        (c) => c !== event.color.hex
+      );
+    } else {
+      this.selectedColors.push(event.color.hex);
+      target.style[
+        'box-shadow'
+      ] = `${event.color.hex}  0px 0px 0px 3px inset, ${event.color.hex}  0px 0px 5px`;
+    }
+
+    console.log(this.selectedColors);
   }
 }
